28742cf183da58415dbb476285fc78a747f21239
Kabajat <kikoabadjiev@gmail.com>
2022-05-16 16:22:40 +0300

    v

--- fw/rend/shaders/fw_shape_shader.h
+++ fw/rend/shaders/fw_shape_shader.h
@@ -2,16 +2,19 @@
 
 static const char *fw_shape_shp_vert =
 "#version 330\n"
+
 "layout(location = 0) in vec2 position;\n"
 "uniform mat4 proj_mat;\n"
 "uniform mat4 model_mat;\n"
 "uniform mat4 tex_mat;\n"
+
 "void main() {\n"
 "    gl_Position = proj_mat * model_mat * vec4(position, 0.0, 1.0);\n"
 "}\0";
 
 static const char *fw_shape_shp_frag =
 "#version 330\n"
+
 "out vec4 out_color;\n"
 "uniform vec4 color;\n"
 "uniform float br;\n"
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "../fw/fw.h"
 
 fw_win_t win;
+fw_rend_t rend;
 
 int main(int32_t argc, char const *argv[]) {
     fw_win_init(
@@ -10,8 +11,13 @@ int main(int32_t argc, char const *argv[]) {
         720,
         0);
 
+    fw_rend_init(&rend, win, 1280 / 3, 720 / 3);
+
     while (fw_is_running(win)) {
         fw_poll_events();
+        fw_rend_begin(&rend);
+        fw_rend_push_rect(&rend, FW_NEW_TRANSFORM(0, 0, 50, 50, 0.5f, 0.5f, 0.0f), FW_COLOR_WHITE);
+        fw_rend_end(&rend);
         fw_swap_buffers(win);
     }
 
