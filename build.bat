a3c85a88157ec613b20f49a64ebb9b97b531b52d
Kabajat <kikoabadjiev@gmail.com>
2022-06-06 21:09:23 +0300

    support for 64bit

--- build.bat
+++
@@ -1,6 +0,0 @@
-set APPLICATION_NAME=FireWingApplication
-set GAME_DIR=src/*.c
-
-set FW_DIR=fw/win/*.c fw/app/*.c fw/rend/*.c fw/input/*.c
-
-gcc -w %GAME_DIR% %FW_DIR% -o bin/win32/%APPLICATION_NAME%.exe -I./win32/include -L./win32/lib -lglfw3dll -lopengl32 -lglu32 -lgdi32 -lglew32 -luser32 -lkernel32
