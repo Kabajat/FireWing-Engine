3fcc53fd72ebdc5f91839d1fbb838d109671d58e
boxDEV <52859306+Kabajat@users.noreply.github.com>
2022-06-16 21:17:05 +0300

    Update fw.h

--- fw/fw.h
+++ fw/fw.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include "math/fw_math.h"
 #include "app/fw_app.h"
 #include "win/fw_win.h"
 #include "rend/fw_rend.h"
 #include "input/fw_input.h"
+
+#ifdef __cplusplus
+}
+#endif
