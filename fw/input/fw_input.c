00ecd00060297e03dc9045ace1374112396feaa8
boxDEV <52859306+Kabajat@users.noreply.github.com>
2022-07-01 00:18:30 +0300

    Update fw_input.c

--- fw/input/fw_input.c
+++ fw/input/fw_input.c
@@ -3,6 +3,9 @@
 FW_API bool fw_is_key_down(fw_win_t *win, int32_t key) { return glfwGetKey(win, key) == GLFW_PRESS; }
 FW_API bool fw_is_mouse_down(fw_win_t *win, int32_t button) { return glfwGetMouseButton(win, button) == GLFW_PRESS; }
 
+FW_API bool fw_is_key_up(fw_win_t *win, int32_t key) { return glfwGetKey(win, key) == GLFW_RELEASE; }
+FW_API bool fw_is_mouse_up(fw_win_t *win, int32_t button) { return glfwGetMouseButton(win, button) == GLFW_RELEASE; }
+
 FW_API void fw_get_mouse_position(fw_win_t *win, int32_t *x, int32_t *y)
 {
     double x_, y_;
