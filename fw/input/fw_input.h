79182ad7a6ba34476efde00e709d8566ccf34c61
boxDEV <52859306+Kabajat@users.noreply.github.com>
2022-07-02 20:43:13 +0300

    Update fw_input.h

--- fw/input/fw_input.h
+++ fw/input/fw_input.h
@@ -214,4 +214,7 @@
 FW_API bool fw_is_key_down(fw_win_t *win, int32_t key);
 FW_API bool fw_is_mouse_down(fw_win_t *win, int32_t button);
 
+FW_API bool fw_is_key_up(fw_win_t *win, int32_t key);
+FW_API bool fw_is_mouse_up(fw_win_t *win, int32_t button);
+
 FW_API void fw_get_mouse_position(fw_win_t *win, int32_t *x, int32_t *y);
