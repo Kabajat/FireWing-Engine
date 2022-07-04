0f96bf5b77d8a72f711a46617275a8672e66aeb1
boxDEV <52859306+Kabajat@users.noreply.github.com>
2022-07-04 22:43:22 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -w src/*.c fw/win/*.c fw/app/*.c fw/rend/*.c fw/input/*.c -o bin/linux/FireWingApplication -I./fw_impl -lglfw -lGLEW -lGL -lm
+gcc -w src/*.c fw_util/win/*.c fw_util/app/*.c fw_util/rend/*.c fw_util/input/*.c -o bin/linux/FireWingApplication -I./fw_impl -lglfw -lGLEW -lGL -lm
