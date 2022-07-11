77617fc343b0920c3cb5bafcb70b1c9fb5c75515
boxDEV <52859306+Kabajat@users.noreply.github.com>
2022-07-11 13:34:03 +0300

    Update fw_input.c

--- fw_util/input/fw_input.c
+++ fw_util/input/fw_input.c
@@ -1,10 +1,24 @@
 #include "fw_input.h"
 
-FW_API bool fw_is_key_down(fw_win_t *win, int32_t key) { return glfwGetKey(win, key) == GLFW_PRESS; }
-FW_API bool fw_is_mouse_down(fw_win_t *win, int32_t button) { return glfwGetMouseButton(win, button) == GLFW_PRESS; }
+FW_API bool fw_is_key_down(fw_win_t *win, int32_t key)
+{
+    return glfwGetKey(win, key) == GLFW_PRESS;
+}
 
-FW_API bool fw_is_key_up(fw_win_t *win, int32_t key) { return glfwGetKey(win, key) == GLFW_RELEASE; }
-FW_API bool fw_is_mouse_up(fw_win_t *win, int32_t button) { return glfwGetMouseButton(win, button) == GLFW_RELEASE; }
+FW_API bool fw_is_mouse_down(fw_win_t *win, int32_t button)
+{
+    return glfwGetMouseButton(win, button) == GLFW_PRESS;
+}
+
+FW_API bool fw_is_key_up(fw_win_t *win, int32_t key)
+{
+    return glfwGetKey(win, key) == GLFW_RELEASE;
+}
+
+FW_API bool fw_is_mouse_up(fw_win_t *win, int32_t button)
+{
+    return glfwGetMouseButton(win, button) == GLFW_RELEASE;
+}
 
 FW_API fw_vec2i_t fw_get_mouse_position(fw_win_t *win)
 {
