14a1abacad6685ddda799d52844ec0416a01f788
boxDEV <52859306+Kabajat@users.noreply.github.com>
2022-07-11 13:33:26 +0300

    Update fw_app.c

--- fw_util/app/fw_app.c
+++ fw_util/app/fw_app.c
@@ -1,9 +1,21 @@
 #include "fw_app.h"
 
-FW_API bool fw_is_running(fw_win_t *win) { return !glfwWindowShouldClose(win); }
+FW_API bool fw_is_running(fw_win_t *win)
+{
+  return !glfwWindowShouldClose(win);
+}
 
-FW_API void fw_swap_buffers(fw_win_t *win) { glfwSwapBuffers(win); }
+FW_API void fw_swap_buffers(fw_win_t *win)
+{
+  glfwSwapBuffers(win);
+}
 
-FW_API void fw_poll_events() { glfwPollEvents(); }
+FW_API void fw_poll_events()
+{
+  glfwPollEvents();
+}
 
-FW_API void fw_kill() { glfwTerminate(); exit(0); }
+FW_API void fw_kill()
+{
+  glfwTerminate();
+}
